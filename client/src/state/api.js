37f704ecf791a4bffaad44a8837a02229bb818fa
Om chaudhary <167326628+omchaudhary007@users.noreply.github.com>
2024-11-10 11:50:48 +0530

    Update api.js

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl:"https://planz-three.vercel.app/",
+    baseUrl:"https://planzserver.onrender.com/",
     credentials: "include",
   }),
   reducerPath: "adminApi",
